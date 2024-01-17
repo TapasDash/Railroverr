efa358f0cbea1745c14d6cac327ff9ace6c2c718
Tapas <tapasdash017@gmail.com>
2024-01-18 04:12:53 +0530

    reqwrites

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8000/:path*", // Proxy to Backend
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
