948a64f19d7d210389534220f0df411d092c20eb
Tapas <tapasdash017@gmail.com>
2024-01-22 08:47:52 +0530

    removed redis

--- app/api/train/dropdown/route.js
+++ app/api/train/dropdown/route.js
@@ -1,19 +1,7 @@
-import redisClient from "@/libs/redisClient";
 import { NextResponse } from "next/server";
-import redisKeys from "@/libs/redisKeys";
 import TrainDetails from "@/models/TrainDetails";
 
 export async function GET(request) {
-  const cachedTrainInfo = await redisClient.get(redisKeys.TRAIN_INFO);
-  if (cachedTrainInfo) {
-    console.log("using cached data");
-    return NextResponse.json({
-      cached: true,
-      success: true,
-      data: JSON.parse(cachedTrainInfo),
-    });
-  }
-
   const trainData = await TrainDetails.aggregate()
     .group({
       _id: "$trainNo",
@@ -24,9 +12,6 @@ export async function GET(request) {
     .project({
       trainName: { $arrayElemAt: ["$trainName", 0] },
     });
-
-  await redisClient.set(redisKey.TRAIN_INFO, JSON.stringify(trainData));
-
   return NextResponse.json({
     success: true,
     data: trainData,
