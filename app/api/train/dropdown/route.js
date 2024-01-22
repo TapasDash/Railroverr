624b428156e9dcb77c9351eb3ddfb787668a6126
Tapas <tapasdash017@gmail.com>
2024-01-22 08:49:57 +0530

    added connect DB

--- app/api/train/dropdown/route.js
+++ app/api/train/dropdown/route.js
@@ -1,7 +1,9 @@
 import { NextResponse } from "next/server";
 import TrainDetails from "@/models/TrainDetails";
+import connectDB from "@/libs/connectDB";
 
 export async function GET(request) {
+  await connectDB();
   const trainData = await TrainDetails.aggregate()
     .group({
       _id: "$trainNo",
