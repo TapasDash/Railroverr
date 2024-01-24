66a14cfdeafe7b4cf43b7e800a9c54b6f79aa38c
Tapas <tapasdash017@gmail.com>
2024-01-24 07:04:22 +0530

    removed: logs

--- app/api/train/dropdown/route.js
+++ app/api/train/dropdown/route.js
@@ -14,7 +14,6 @@ export async function GET(request) {
     .project({
       trainName: { $arrayElemAt: ["$trainName", 0] },
     });
-  console.log("is");
   return NextResponse.json({
     success: true,
     data: trainData,
