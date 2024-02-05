cd06659e12c93612940b268d55fbd3a166db8862
Tapas <tapasdash017@gmail.com>
2024-02-05 07:31:19 +0530

    fixed casing of lottie animations

--- utils/NotFound/NotFoundLoader.jsx
+++ utils/NotFound/NotFoundLoader.jsx
@@ -1,9 +1,9 @@
 import Lottie from "lottie-react";
 import notFound from "./notFound.json";
 
-import "../../styles/NotFoundLoader.scss";
+// import "../../styles/notFoundLoader.scss";
 
-// import "@/styles/NotFoundLoader.scss";
+import "@/styles/notFoundLoader.scss";
 
 const NotFoundLoader = () => (
   <Lottie animationData={notFound} loop={true} id="loader" />
--- utils/TrainLoader/TrainLoader.jsx
+++ utils/TrainLoader/TrainLoader.jsx
@@ -1,8 +1,8 @@
 import Lottie from "lottie-react";
 import train from "./train.json";
 
-import "../../styles/trainLoader.scss";
-// import "@/styles/trainLoader.scss";
+// import "../../styles/trainLoader.scss";
+import "@/styles/trainLoader.scss";
 
 const TrainLoader = () => (
   <Lottie animationData={train} loop={true} id="loader" />
