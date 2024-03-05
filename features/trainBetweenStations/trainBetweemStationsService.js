407fd229023018496db8dd7e6e527486cab48612
Tapas <tapasdash017@gmail.com>
2024-03-05 16:06:44 +0530

    aade

--- features/trainBetweenStations/trainBetweemStationsService.js
+++ features/trainBetweenStations/trainBetweemStationsService.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const getTrainBetweenStations = async (fromStation, toStation) => {
   console.log("getTrainBetweenStations", { fromStation, toStation });
-  console.log("new env---<", process.env.NEXT_PUBLIC_TRAIN_TIMETABLE_URL);
+  console.log("new env--->", process.env.NEXT_PUBLIC_TRAIN_TIMETABLE_URL);
   const response = await axios.get(
     `${process.env.NEXT_PUBLIC_TRAIN_TIMETABLE_URL}/from/${fromStation}/to/${toStation}`
   ); // { stationCode: fromStation, destinationStation: toStation }
