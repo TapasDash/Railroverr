733f628ad321e330c797dd4ab881d766194fa4c9
Tapas <tapasdash017@gmail.com>
2024-10-21 07:03:41 +0530

    fix: mongo connect db

--- libs/connectDB.js
+++ libs/connectDB.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected...`);
   } catch (err) {
     console.log(err.message);
