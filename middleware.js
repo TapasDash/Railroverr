0fd57dc8cd4b01f06f4fcf0285f9bc2ece88e622
Tapas <tapasdash017@gmail.com>
2025-01-26 10:00:11 +0530

    feat: add CORS middleware for API requests

---
+++ middleware.js
@@ -0,0 +1,29 @@
+// middleware.js
+import { NextResponse } from "next/server";
+
+export function middleware(req) {
+  const response = NextResponse.next(); // Continue processing the request
+
+  // Add CORS headers to the response
+  response.headers.set("Access-Control-Allow-Origin", [
+    "http://localhost:3000",
+    "https://railroverr.vercel.app",
+    "https://railrover.vercel.app",
+    "https://railroverr.vercel.app/api/train",
+  ]); // Your frontend domain
+  response.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  response.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+
+  return response;
+}
+
+// Optional: Matcher to apply middleware to specific paths
+export const config = {
+  matcher: "/api/:path*", // Apply middleware to all paths under /api
+};
